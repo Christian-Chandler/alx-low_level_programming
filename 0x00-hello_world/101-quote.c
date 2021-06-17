da2f5bf3145a20a3ef15ed319f200c2d65d4b117
root <cccchima>
2021-06-17 13:25:12 -0700

    Print using WRITE

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+/**
+ * main - Prints a string using the "write" function defined in 
+ *        the UNIX standard libraries.
+ *
+ * Return: Always 1 to indicate success.
+ */
+int main(void)
+{
+	write(1,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      60);
+
+	return (1);
+}
