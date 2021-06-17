1883d4e684d0bf3f5aa2c13878efe112175f3bf2
root <cccchima>
2021-06-17 11:41:10 -0700

    size of

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints the sizes of the various "types" in the computer which "this" will compile or run.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s: %d byte(s)\n", "Size of char", sizeof (char));
+	printf("%s: %d byte(s)\n", "Size of an int", sizeof (int));
+	printf("%s: %d byte(s)\n", "Size of a long int", sizeof (long int));
+	printf("%s: %d byte(s)\n", "Size of a long long int", sizeof (long long int));
+	printf("%s: %d byte(s)\n", "Size of a float", sizeof (float));
+
+	return (0);
+	
+}
