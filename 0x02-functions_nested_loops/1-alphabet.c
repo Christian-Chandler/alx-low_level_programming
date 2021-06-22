a8b0d3f54b4e48779a569658719a2e1cf6f092d8
root <cccchima>
2021-06-22 11:32:57 -0700

    Print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_alphabet - Prints the alphabets in lower case followed
+ *                  by a new line.
+ *
+ * Return: Ommitted because of 'void'.
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+
+	_putchar(\n);
+}
