67702d005c5e45af4be1362c968c19e3f97ab40c
root <cccchima>
2021-06-22 11:51:04 -0700

    print 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_alphabet - Prints the alphabets in lower case x10 followed
+ *                  by a new line.
+ *
+ * Return: Ommitted because of 'void'.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 1; i <=10; i++)
+	{
+	
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	}
+}
