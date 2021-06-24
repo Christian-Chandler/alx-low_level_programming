f450f91bcda355b108a508105393bb3fc521d40d
root <cccchima>
2021-06-24 10:36:33 -0700

    print most

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Prints from 0 to 9  except 2 and 4 followed by a new line
+ */
+
+void print_most_numbers(void)
+{
+	int z;
+
+	for (z = 0; z <= 9; z++)
+	{
+		if (z != 2 || z != 4)
+
+		_putchar((z % 10) + '0');
+	}
+
+	_putchar('\n');
+}
