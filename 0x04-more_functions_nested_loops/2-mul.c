70396d00325ab76f602241954a0e99aa73b8408a
root <cccchima>
2021-06-24 09:17:58 -0700

    Multiply 2 integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: First integer
+ * @b: second integer
+ * Return: product value
+ */
+
+int mul(int a, int b)
+{
+	int prod = a * b;
+
+	return (prod);
+}
