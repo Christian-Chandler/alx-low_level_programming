c1af4383831eebb15bfff6bdfd027305c73043b6
root <cccchima>
2021-07-01 05:49:46 -0700

    Compare Strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,23 +5,28 @@
  * @s1: Points to 1st string
  * @s2: Points to second string
  *
- * Return: -15 if less, 0 if equal or 15 if greater
+ * Return: -1 if less, 0 if equal or 1 if greater
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 1;
-	int j = 1;
+	int i = 0;
+	int j = 0;
 
-	while (s1[i] != '\0')
+	do {
 		i++;
-
-	while (s2[j] != '\0')
 		j++;
+	} while (s1[i] != '\0' && s2[j] != '\0');
 
-	if (i < j && i >= 1)
-		return (-15);
-	else if (i > j && i>= 1)
-		return (15);
+	if (s1[i] == '\0' && s2[j] != '\0')
+	{
+		j++;
+		return (i - j);
+	}
+	else if (s1[i] != '\0' && s2[j] == '\0')
+	{
+		i++;
+		return (i - j);
+	}
 	else
 		return (0);
 }
