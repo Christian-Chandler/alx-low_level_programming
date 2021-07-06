b97f68a540db3165dc95964112fb14ea9cc6ba6a
root <cccchima>
2021-07-06 10:45:28 -0700

    setmemory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _memset - fills memory with a constant byte.
+ * @s: Pointer to memory area.
+ * @b: Constant byte character.
+ * @n: Number of bytes to be filled.
+ * Return:s.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*s = b;
+		s++;
+	}
+	return (s);
+}
