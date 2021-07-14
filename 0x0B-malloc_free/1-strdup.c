dd98d66080d6c2e82068145e3dda72e6e2b3675e
root <cccchima>
2021-07-14 13:18:55 -0700

    duplicate string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - Returns a pointer to a newly-allocated space in memory
+ *           containing a copy of the string given as parameter.
+ * @str: The string to be copied.
+ *
+ * Return: If str == NULL or insufficient memory is available - NULL.
+ *         Otherwise - a pointer to the duplicated string.
+ */
+char *_strdup(char *str)
+{
+	char *dup_str;
+	int i = 0, len;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = strlen(str);
+
+	dup_str = malloc(sizeof(char) * len);
+
+	if (dup_str != NULL)
+	{
+		for (i = 0; str[i] != '\0'; i++)
+			dup_str[i] = str[i];
+		dup_str[i] = '\0';
+
+		return (dup_str);
+	}
+	else
+		return (NULL);
+}
