a0f1a38aa5ecdb2ecc04d68b5298b5b2c172b6f8
root <cccchima>
2021-07-14 13:08:45 -0700

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * create_array - Creates an array of chars and
+ *                initializes it with a specific char.
+ * @size: The size of the array to be initialized.
+ * @c: The specific char to intialize the array with.
+ *
+ * Return: If size == 0 or the function fails - NULL.
+ *         Otherwise - a pointer to the array.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *new_array;
+	unsigned int i = 0;
+
+	if (size == 0)
+		return (NULL);
+	new_array = malloc(sizeof(char) * size);
+
+	if (new_array != NULL)
+	{
+		while (i < size)
+		{
+			new_array[i] = c;
+			i++;
+		}
+		return (new_array);
+	}
+	else
+		return (NULL);
+}
