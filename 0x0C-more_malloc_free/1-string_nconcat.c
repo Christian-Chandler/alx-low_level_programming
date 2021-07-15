7816e5ccf2fc6e95ef426bd94e15848ecb0073d5
root <cccchima>
2021-07-15 09:28:42 -0700

    concat string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,23 +31,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		cat_str[i] = s1[i];
 	}
-	if (n >= strlen(s2))
+	for (j = 0; j < n; j++)
 	{
-		for (j = 0; s2[j] != '\0'; j++)
-		{
-			cat_str[i] = s2[j];
-			i++;
-		}
-		cat_str[i] = '\0';
+		cat_str[i] = s2[j];
+		i++;
 	}
-	else
-	{
-		for (j = 0; j < n; j++)
-		{
-			cat_str[i] = s2[j];
-			i++;
-		}
-		cat_str[i] = '\0';
-	}
+	cat_str[i] = '\0';
 	return (cat_str);
 }
