7d6adfe6c30e3d08db521d6d718c0ef943d430fb
root <cccchima>
2021-07-20 16:43:29 -0700

    Create new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
-
+#include <string.h>
 /**
  * new_dog - creates a new dog.
  * @name: New dog name.
@@ -10,18 +10,33 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_puppy;
-	char __attribute__((unused)) *name_cpy, *owner_cpy;
+	dog_t *new_dog;
 
-	name_cpy = name;
-	owner_cpy = owner;
-	new_puppy = malloc(sizeof(dog_t));
-
-	if (new_puppy == NULL)
+	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
-	new_puppy->name = name;
-	new_puppy->age = age;
-	new_puppy->owner = owner;
 
-	return (new_puppy);
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+
+	new_dog->name = malloc((sizeof(char)) * (sizeof(name)));
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+
+	new_dog->owner = malloc((sizeof(char)) * (sizeof(owner)));
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+
+	new_dog->name = strcpy(new_dog->name, name);
+	new_dog->age = age;
+	new_dog->owner = strcpy(new_dog->owner, owner);
+
+	return (new_dog);
 }
