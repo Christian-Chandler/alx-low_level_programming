2c6a569840a923c937580573f6e56adaa41960f2
root <cccchima>
2021-07-20 06:58:25 -0700

    README and struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define a new type struct dog
+ * @name: Dog name.
+ * @age: Dog age.
+ * @owner: Dog owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
