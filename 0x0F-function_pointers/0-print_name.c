03ebf59a4ec833fb8d0270954ae55bcbc56fbb3d
root <cccchima>
2021-07-21 19:21:04 -0700

    Print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name -  prints a name.
+ * @name: Name to be printed.
+ * @f: the fxn the name is to be printed with.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
